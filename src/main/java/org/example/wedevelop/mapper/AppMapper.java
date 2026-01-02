385b04b23be35a9320209b626279ed4fbf5c1c63
XiaoMaColtAI <2503425043@qq.com>
2026-01-02 19:31:48 +0800

    后端： - 实现应用实体类、DTO、VO以及相关服务层和控制器 - 实现应用增删改查、分页查询和精选应用功能 - 实现管理员权限校验和管理员特殊操作接口

---
+++ src/main/java/org/example/wedevelop/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package org.example.wedevelop.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import org.example.wedevelop.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/SeeWorldOr">SeeWorldOr</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/org/example/wedevelop/service/AppService.java
@@ -0,0 +1,42 @@
+package org.example.wedevelop.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import org.example.wedevelop.model.dto.app.AppQueryRequest;
+import org.example.wedevelop.model.entity.App;
+import org.example.wedevelop.model.vo.AppVO;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/SeeWorldOr">SeeWorldOr</a>
+ */
+public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+}
